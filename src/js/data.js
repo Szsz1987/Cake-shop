0c5ef7a9a744e2e92069604dca9be847b801857c
Szymon Szuliński <sszulinski@gmail.com>
2022-10-11 22:49:27 +0200

    Init project

---
+++ src/js/data.js
@@ -0,0 +1,87 @@
+const dataSource = {}; // eslint-disable-line no-unused-vars
+
+dataSource.products = {
+  yyy1: {
+    class: '',
+    name: '',
+    price: 0,
+    description: '',
+    images: [
+      '<img class="active" src="">',
+    ],
+  },
+  yyy2: {
+    class: '',
+    name: '',
+    price: 0,
+    description: '',
+    images: [
+      '<img class="active" src="">',
+    ],
+    yyy3: {
+      yyy4: {
+        label: '',
+        type: 'radios',
+        options: {
+          yyy5: {label: '', price: 0, default: true},
+          yyy6: {label: '', price: 0},
+        },
+      },
+    },
+  },
+  yyy7: {
+    name: '',
+    price: 0,
+    description: '',
+    images: [
+      '<img class="active" src="">',
+      '<img class="active xxx" src="">',
+    ],
+    params: {
+      yyy8: {
+        label: '',
+        type: 'radios',
+        options: {
+          yyy9: {label: '', price: 0, default: true},
+          yyy10: {label: '', price: 0},
+        },
+      },
+      yyy11: {
+        label: '',
+        type: 'checkboxes',
+        options: {
+          yyy12: {label: '', price: 0, default: true},
+          yyy13: {label: '', price: 0},
+        },
+      },
+      yyy14: {
+        label: '',
+        type: 'select',
+        options: {
+          yyy15: {label: '', price: 0, default: true},
+          yyy16: {label: '', price: 0},
+        },
+      },
+    },
+  },
+  yyy17: {
+    name: '',
+    price: 0,
+    description: '',
+    images: [
+      '<img class="active" src="">',
+      '<img class="active xxx" src="" style="z-index: 1;">',
+    ],
+    params: {
+      yyy18: {
+        label: '',
+        type: 'checkboxes',
+        options: {
+          yyy19: {label: '', price: 0, default: true},
+          yyy20: {label: '', price: 0},
+        },
+      },
+    },
+  },
+
+};
